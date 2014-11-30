328d86dbdd0268c0181140d51486bfe7fa8dcc8b
stroki <kinebogin@gmail.com>
2014-11-30 14:42:03 +0400

    Finnaly working

--- src/main/scala/com/github/kantefier/weatherparse/WeatherParser.scala
+++ src/main/scala/com/github/kantefier/weatherparse/WeatherParser.scala
@@ -1,20 +1,56 @@
 package com.github.kantefier.weatherparse
 
+import java.util.Calendar
+
 import scala.util.parsing.combinator.RegexParsers
+import java.io._
+import scala.io.Source
 import MatrixHelper._
 
 object WeatherParser {
+
+  val workingDirPath = "C:\\WeatherParser"
+  val dataFilename = "cetdl1772on.dat"
+  val resultFilename = s"output_${Calendar.getInstance().getTimeInMillis % 10000}.dat"
+
   def main (args: Array[String]): Unit = {
-    ???
+    val dataInputFile = new File(s"$workingDirPath\\$dataFilename")
+    val dataOutputFile = new File(s"$workingDirPath\\$resultFilename")
+
+    if(!dataInputFile.exists())
+      throw new Exception(s"Data file not found: $workingDirPath\\$dataFilename")
+
+    val dataSource = Source.fromFile(dataInputFile).getLines()
+    val dataDestination = new PrintWriter(dataOutputFile)
+    var linecounter = 0
+    val linesBuffer = new StringBuilder
+
+    while(dataSource.hasNext) {
+      linecounter += 1
+      linesBuffer.append(dataSource.next())
+      if(linecounter % 31 == 0) {
+        val yearDataMatrix = linesBuffer.mkString.lines.map(line => SimpleLineParser.parseAll(SimpleLineParser.lineOfData, line)).map{
+          case SimpleLineParser.Success(resultRow, _) => resultRow
+          case SimpleLineParser.Failure(_, _) => Nil
+          case SimpleLineParser.Error(_, _) => Nil
+        }.toList
+        dataDestination.println(parseOneYearData(yearDataMatrix).mkString("\n"))
+        linesBuffer.setLength(0)
+      } else {
+        linesBuffer.append(System.lineSeparator())
+      }
+    }
+    dataDestination.flush()
+    dataDestination.close()
   }
 
-  def parseOneYearData(dataMatrix: Matrix): List[(String, Int)] = {
+  def parseOneYearData(dataMatrix: Matrix): List[String] = {
     val (year, yearlessMatrix) = (dataMatrix.T.head.head, dataMatrix.T.tail)
     val (days, dataByMonthsMatrix) = (yearlessMatrix.head, yearlessMatrix.zipWithIndex.tail)
     val dataByDaysWithMonth = dataByMonthsMatrix.map(dataWithMonthNumber => (dataWithMonthNumber._1.zip(days), dataWithMonthNumber._2))
     val resultSet = dataByDaysWithMonth.flatMap(monthDataTuple => {
       val (dataWithDays, monthNumb) = monthDataTuple
-      dataWithDays.flatMap(dayData => if(dayData._1 != -999) Some((s"${dayData._2}/${monthNumb}/${year}", dayData._1)) else None)
+      dataWithDays.flatMap(dayData => if(dayData._1 != -999) Some(s"${dayData._2}/$monthNumb/$year ${dayData._1}") else None)
     })
     resultSet
   }
